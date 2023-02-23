7841c88c8dd918123b7805670f4d5c413ea4085a
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-02-23 11:52:41 +0400

    initialized list controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,7 @@
+class MoviesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'movies/new'
+  get 'movies/create'
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+
+  resources :lists # , except: [:index]
+  resources :movies, only: [:index]
 end
